785974c2cc4bb75776e1bbb34c66be54579ad1a0
Thomas Bateson <thomas.bateson@eagles.oc.edu>
2019-10-19 00:18:05 -0500

    Refactored partials to work with different Contexts

--- src/main/java/horseshoe/Partials.java
+++ src/main/java/horseshoe/Partials.java
@@ -10,27 +10,33 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public final class Partials {
 
-	interface LazyTemplate {
-		Template get(Context context) throws IOException, LoadException;
+	static abstract class LazyTemplate {
+		final Map<Context, Template> values = new HashMap<>();
+		final Template get(Context context) throws IOException, LoadException {
+				Template t = values.get(context);
+				if (t == null) {
+					values.put(context, Template.RECURSIVE_TEMPLATE_DETECTED);
+					t = createNewTemplate(context);
+					values.put(context, t);
+				}
+				return t;
+		}
+		abstract Template createNewTemplate(Context context) throws IOException, LoadException;
 	}
 
 	final Map<String, LazyTemplate> map = new LinkedHashMap<>();
 
 	public Partials add(final String name, final String partial) {
 		map.put(name, new LazyTemplate() {
-			private Template t = null;
 			@Override
-			public synchronized Template get(final Context context) throws IOException, LoadException {
-				if (t == null) {
-					t = Template.RECURSIVE_TEMPLATE_DETECTED;
-					t = new Template(name, partial, context);
-				}
-				return t;
+			public synchronized Template createNewTemplate(final Context context) throws IOException, LoadException {
+				return new Template(name, partial, context);
 			}
 		});
 		return this;
@@ -38,20 +44,25 @@ public final class Partials {
 
 	public Partials add(final String name, final Reader reader) {
 		map.put(name, new LazyTemplate() {
-			private Template t = null;
 			@Override
-			public synchronized Template get(final Context context) throws IOException, LoadException {
-				if (t == null) {
-					t = Template.RECURSIVE_TEMPLATE_DETECTED;
-					t = new Template(name, reader, context);
-				}
-				return t;
+			public synchronized Template createNewTemplate(final Context context) throws IOException, LoadException {
+				return new Template(name, reader, context);
 			}
 		});
 		return this;
 	}
 
-	public Partials directory(final Path directory, final Charset charset, final String fileExtension) throws IOException {
+	public Partials add(final String name, final Path file, final Charset charset) {
+		map.put(name, new LazyTemplate() {
+			@Override
+			public synchronized Template createNewTemplate(final Context context) throws IOException, LoadException {
+				return new Template(name, file, charset, context);
+			}
+		});
+		return this;
+	}
+
+	public Partials addDirectory(final Path directory, final Charset charset, final String fileExtension) throws IOException {
 		Files.walkFileTree(directory, EnumSet.noneOf(FileVisitOption.class), 1, new SimpleFileVisitor<Path>() {
 			@Override
 			public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) {
@@ -60,29 +71,13 @@ public final class Partials {
 				if (f != null && (fileName = f.toString()).endsWith(fileExtension)) {
 					final String fileNameNoExtension = fileName.substring(0, fileName.lastIndexOf('.'));
 					map.put(fileNameNoExtension, new LazyTemplate() {
-						private Template t = null;
 						@Override
-						public synchronized Template get(final Context context) throws IOException, LoadException {
-							if (t == null) {
-								t = Template.RECURSIVE_TEMPLATE_DETECTED;
-								t = new Template(fileNameNoExtension, file, charset, context);
-							}
-							return t;
+						public synchronized Template createNewTemplate(final Context context) throws IOException, LoadException {
+							return new Template(fileNameNoExtension, file, charset, context);
 						}
 					});
 				}
 				return FileVisitResult.CONTINUE;
-			}
-		});
-		return this;
-	}
-
-	public Partials add(final String name, final Template template) {
-		// TODO: seems bizarre to have to specify the name of a template when its an argument (either implicitly through the file name or explicitly)
-		map.put(name, new LazyTemplate() {
-			@Override
-			public Template get(final Context context) {
-				return template;
 			}
 		});
 		return this;
